c902f50c90a89f00325bfaa2e70019449462d6c9
Singee <git@singee.me>
2021-04-13 11:03:37 +0800

    增加对于 float 的转换支持

--- reflect.go
+++ reflect.go
@@ -1,6 +1,7 @@
 package dt
 
 import (
+	"math"
 	"reflect"
 )
 
@@ -12,6 +13,8 @@ func MapReflectType(p reflect.Kind) Type {
 		return NumberType
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		return NumberType
+	case reflect.Float64, reflect.Float32:
+		return NumberType
 	case reflect.String:
 		return StringType
 	default:
@@ -22,16 +25,31 @@ func MapReflectType(p reflect.Kind) Type {
 var invalidReflectValue = reflect.Value{}
 
 var reflectTypes = map[reflect.Kind]reflect.Type{
-	reflect.Int:    reflect.TypeOf(1),
-	reflect.Int8:   reflect.TypeOf(int8(1)),
-	reflect.Int16:  reflect.TypeOf(int16(1)),
-	reflect.Int32:  reflect.TypeOf(int32(1)),
-	reflect.Int64:  reflect.TypeOf(int64(1)),
-	reflect.Uint:   reflect.TypeOf(uint(1)),
-	reflect.Uint8:  reflect.TypeOf(uint8(1)),
-	reflect.Uint16: reflect.TypeOf(uint16(1)),
-	reflect.Uint32: reflect.TypeOf(uint32(1)),
-	reflect.Uint64: reflect.TypeOf(uint64(1)),
+	reflect.Int:     reflect.TypeOf(1),
+	reflect.Int8:    reflect.TypeOf(int8(1)),
+	reflect.Int16:   reflect.TypeOf(int16(1)),
+	reflect.Int32:   reflect.TypeOf(int32(1)),
+	reflect.Int64:   reflect.TypeOf(int64(1)),
+	reflect.Uint:    reflect.TypeOf(uint(1)),
+	reflect.Uint8:   reflect.TypeOf(uint8(1)),
+	reflect.Uint16:  reflect.TypeOf(uint16(1)),
+	reflect.Uint32:  reflect.TypeOf(uint32(1)),
+	reflect.Uint64:  reflect.TypeOf(uint64(1)),
+	reflect.Float32: reflect.TypeOf(float32(1)),
+	reflect.Float64: reflect.TypeOf(float64(1)),
+}
+
+func CheckOverflowFloat(x float64, k reflect.Kind) bool {
+	switch k {
+	case reflect.Float32:
+		if x < 0 {
+			x = -x
+		}
+		return math.MaxFloat32 < x && x <= math.MaxFloat64
+	case reflect.Float64:
+		return false
+	}
+	panic("invalid kind")
 }
 
 func CheckOverflowInt(x int64, k reflect.Kind) bool {
@@ -94,6 +112,16 @@ func ConvertToReflectType(v Value, toType reflect.Kind) (value reflect.Value, ok
 			}
 			vvv := vv.UInt64()
 			if CheckOverflowUInt(vvv, toType) {
+				return invalidReflectValue, false
+			}
+
+			return reflect.ValueOf(vvv).Convert(reflectTypes[toType]), true
+		case reflect.Float32, reflect.Float64:
+			if !vv.IsFloat64() {
+				return invalidReflectValue, false
+			}
+			vvv := vv.Float64()
+			if CheckOverflowFloat(vvv, toType) {
 				return invalidReflectValue, false
 			}
 
