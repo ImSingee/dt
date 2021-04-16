5c9b53b3f3ca208d08d054a77ec8c8e687f396ca
Singee <git@singee.me>
2021-04-16 10:38:45 +0800

    增加 ToStringer 方法

--- string.go
+++ string.go
@@ -5,8 +5,12 @@ import (
 	"strconv"
 )
 
-type Stringer interface {
-	String() string
+type Stringer = fmt.Stringer
+
+type String string
+
+func (s String) String() string {
+	return string(s)
 }
 
 func ToString(v interface{}) string {
@@ -23,5 +27,14 @@ func ToString(v interface{}) string {
 		return strconv.FormatFloat(vv, 'f', -1, 64)
 	default:
 		return fmt.Sprintf("%#+v", v)
+	}
+}
+
+func ToStringer(v interface{}) Stringer {
+	switch vv := v.(type) {
+	case Stringer:
+		return vv
+	default:
+		return String(ToString(v))
 	}
 }
